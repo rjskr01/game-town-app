d940c26fbdd70d6ebb81db3dbf90241e3da93d15
rjskr01 <rjskr01@gmail.com>
2023-09-02 00:04:10 +0530

    simple next js sample was committed.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <main className="container">
+        <div className="flex">
+          <h1> Hello world</h1>
+      
+        </div>
+      </main>
+    )
+  }
