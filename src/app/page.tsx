f69139ebf6350165ba88d081fb696ff428dbe540
rjskr01 <rjskr01@gmail.com>
2023-09-05 21:50:49 +0530

    Login component was included

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Login from "./component/login"
 import MainNavigation from "./component/main-navigation"
 
 export default function Home() {
     return (
-      <main className="container">
+      <main className="">
         <div className="flex flex-row">
           <section className="flex-[60%] ml-[50px]">
             <MainNavigation/>
+            <Login/>
           </section>
           <section className="flex-[40%] mt-[20px]">
 
