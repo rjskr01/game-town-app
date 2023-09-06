fa573cd6d77e57e61fbb4aaf3384e5722706d069
rjskr01 <rjskr01@gmail.com>
2023-09-06 21:12:49 +0530

    login input fields style correction was committed

--- src/app/component/game-category.tsx
+++ src/app/component/game-category.tsx
@@ -3,7 +3,7 @@ import GameCategoryMenuNavigation from "./game-category-menu-navigation";
 const GameCategory = () => {
     return (
         <div id="gameCategory" className="w-[90%] h-[calc(100vh-200px)] bg-white">
-            <div id="gameCategoryMenu" className="flex flex-row font-[800] text-black p-[14px] text-[13px] font-[Arial] ">
+            <div id="gameCategoryMenu" className="flex flex-row font-[800] text-black p-[14px] text-[13px] font-[Arial] mt-[20px]">
                 <div className="mr-[20px]">
                     GAME CATEGORIES:
                 </div>
